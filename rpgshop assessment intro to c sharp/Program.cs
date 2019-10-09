501f846697950a965a409ce522a5c764849feaee
Sean Eastin <s198013@students.aie.edu>
2019-10-09 12:49:10 -0500

    added documentation

--- rpgshop assessment intro to c sharp/Program.cs
+++ rpgshop assessment intro to c sharp/Program.cs
@@ -12,7 +12,6 @@ namespace rpgshop_assessment_intro_to_c_sharp
         static void Main(string[] args)
         {
                Game game = new Game(); //creates a new instance of the game
-           /* if () */
             game.start(); // goes to set up all the weapons 
         }
 
